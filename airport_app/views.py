ad254c914f280fc129770ad076a36890421b7809
numer <5079447@gmail.com>
2024-04-02 02:51:14 +0300

    implemented CountryViewSet & CityViewSet

--- airport_app/views.py
+++ airport_app/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from airport_app.models import (
+    Country,
+    City,
+)
+from airport_app.serializers import (
+    CountrySerializer,
+    CitySerializer,
+    CityListSerializer
+)
+
+
+class CountryViewSet(ModelViewSet):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+
+
+class CityViewSet(ModelViewSet):
+    queryset = City.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return CityListSerializer
+
+        return CitySerializer
