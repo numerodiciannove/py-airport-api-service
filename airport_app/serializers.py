4ebccf9da6625a6b747e534eefc8ee0956dff167
numer <5079447@gmail.com>
2024-04-02 02:50:52 +0300

    implemented CountrySerializer & CitySerializer

---
+++ airport_app/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+
+from airport_app.models import (
+    Country,
+    City,
+)
+
+
+class CountrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = "__all__"
+
+
+class CitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = City
+        fields = ("id", "name", "country")
+
+
+class CityListSerializer(CitySerializer):
+    country = CountrySerializer()
