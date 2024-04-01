25179b6bd5fa3b0a2c021788a75519e6de1bcee7
numer <5079447@gmail.com>
2024-04-01 22:07:59 +0300

    fix models verbose names

--- airport_app/models.py
+++ airport_app/models.py
@@ -7,6 +7,9 @@ from airport_service import settings
 class Country(models.Model):
     name = models.CharField(max_length=255)
 
+    class Meta:
+        verbose_name_plural = "countries"
+
     def __str__(self):
         return self.name
 
@@ -14,6 +17,9 @@ class Country(models.Model):
 class City(models.Model):
     name = models.CharField(max_length=255)
     country = models.ForeignKey(Country, on_delete=models.CASCADE, )
+
+    class Meta:
+        verbose_name_plural = "cities"
 
     def __str__(self):
         return self.name
